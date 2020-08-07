c0420e07ebb4a02c78f896890c51442ea82ed99c
Oat <oatyootna@gmail.com>
2020-08-07 22:43:58 +0700

    Second Try

--- fibonacci.go
+++ fibonacci.go
@@ -6,37 +6,24 @@ func fibonacci() func() int {
 
 	firstDigit := 0
 	secondDigit := 1
-	i := -1
-	sum := 0
+	sum := -1
 
 	return func() int {
 
-		i++
-		if i > 1 {
+		sum++
+		if sum > 1 {
 
 			sum = firstDigit + secondDigit
 			firstDigit = secondDigit
 			secondDigit = sum
 
 			return sum
-
 		}
-
-		if i == 0 {
-
-			return 0
-
-		} else {
-
-			return 1
-
-		}
-
+		return sum
 	}
 }
 
 func main() {
-
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
 		fmt.Println(f())
